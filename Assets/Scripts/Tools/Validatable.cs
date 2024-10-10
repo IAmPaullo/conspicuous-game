77770ad185d070d09f3596e5ad0f7db311741b11
Paullo Bahia <paullotsw@gmail.com>
2024-10-09 23:50:18 -0300

    fixed exception message

--- Assets/Scripts/Tools/Validatable.cs
+++ Assets/Scripts/Tools/Validatable.cs
@@ -6,12 +6,12 @@ public abstract class Validatable : MonoBehaviour
     protected void ValidateObject(object obj, string objName)
     {
         if (obj == null)
-            throw new System.Exception($"{objName} não foi atribuído corretamente.");
+            throw new System.Exception($"{objName} was not correctly defined.");
     }
 
     protected void ValidateList<T>(IList<T> list, string listName)
     {
         if (list == null || list.Count == 0)
-            throw new System.Exception($"A lista {listName} está nula ou vazia.");
+            throw new System.Exception($"The List {listName} is null or empty.");
     }
 }
